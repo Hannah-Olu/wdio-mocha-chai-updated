51d19d10bbf33f4f1ef7a70efd46549e47e67a83
Hannah <hannah.olujekun@gmail.com>
2018-10-18 10:05:59 +0100

    added new homepage and remove-overlay

---
+++ page-objects/new-homepage.js
@@ -0,0 +1,9 @@
+class newHomepage {
+    get logo() {
+        return $(".navbar-brand")
+    }
+
+    get getInTouch() { return $("a[href='https://ecs-digital.co.uk/contact']") }
+}
+
+module.exports = new newHomepage()
